bbccd75f16b63e6249fd1a899e9aa12fa73e9dec
Scott Radcliff <radcliffsc@gmail.com>
2016-08-27 19:08:39 -0400

    refactor

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,43 @@
+// Set event handlers for the action links: start, pause, stop
 document.querySelector("[data-attribute='start']").addEventListener('click', start);
 document.querySelector("[data-attribute='pause']").addEventListener('click', pause);
+document.querySelector("[data-attribute='stop']").addEventListener('click', stop);
 
 var firstSecond = false;
 var countdownInterval;
+var timer = document.querySelector("[data-attribute='countdown']");
 
 function start(event) {
   event.preventDefault();
   startCountdown();
 }
 
+function clear() {
+  clearInterval(countdownInterval);
+}
+
+function resetTimer() {
+  timer.innerHTML = "25:00";
+  document.title = "25:00";
+}
+
+function stop(event) {
+  event.preventDefault();
+  resetTimer();
+  clear();
+} 
 
 function startCountdown() {
   countdownInterval = setInterval(reduceTime, 1000);
 }
 
-
 function pause(event) {
   event.preventDefault();
-  clearInterval(countdownInterval);
+  clear();
 }
 
 
 function reduceTime() {
-  var timer = document.querySelector("[data-attribute='countdown']");
-
   if (timer.innerHTML == "25:00" &&  firstSecond === false) {
     firstSecond = true;
   } else if (timer.innerHTML == "25:00" && firstSecond === true) {
@@ -35,8 +49,14 @@ function reduceTime() {
     var seconds = minutesAndSeconds[1];
 
     if (seconds == "00") {
-      minutes = parseInt(minutes) - 1;
-      seconds = "59";
+      if (minutes == "0") {
+        resetTimer();
+        clear();
+        return;
+      } else {
+        minutes = parseInt(minutes) - 1;
+        seconds = "59";
+      }
     } else {
       seconds = parseInt(seconds) - 1;
     }
