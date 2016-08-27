4c24ab37be577dfe6d256e1dd51f430767134408
Scott Radcliff <radcliffsc@gmail.com>
2016-08-27 18:28:14 -0400

    Wire up start and pause links

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,8 @@
 document.querySelector("[data-attribute='start']").addEventListener('click', start);
+document.querySelector("[data-attribute='pause']").addEventListener('click', pause);
 
 var firstSecond = false;
+var countdownInterval;
 
 function start(event) {
   event.preventDefault();
@@ -9,22 +11,39 @@ function start(event) {
 
 
 function startCountdown() {
-  setInterval(reduceTime, 1000);
+  countdownInterval = setInterval(reduceTime, 1000);
+}
+
+
+function pause(event) {
+  event.preventDefault();
+  clearInterval(countdownInterval);
 }
 
 
 function reduceTime() {
   var timer = document.querySelector("[data-attribute='countdown']");
+
   if (timer.innerHTML == "25:00" &&  firstSecond === false) {
     firstSecond = true;
   } else if (timer.innerHTML == "25:00" && firstSecond === true) {
     timer.innerHTML = "24:59";
     firstSecond = false;
   } else {
-    var time = timer.innerHTML;
-    var hoursAndSeconds = timer.innerHTML.split(":");
-    hoursAndSeconds[1] = parseInt(hoursAndSeconds[1]) - 1;
-    timer.innerHTML = hoursAndSeconds[0] + ":" + hoursAndSeconds[1];
-    document.title = hoursAndSeconds[0] + ":" + hoursAndSeconds[1];
+    var minutesAndSeconds = timer.innerHTML.split(":");
+    var minutes = minutesAndSeconds[0];
+    var seconds = minutesAndSeconds[1];
+
+    if (seconds == "00") {
+      minutes = parseInt(minutes) - 1;
+      seconds = "59";
+    } else {
+      seconds = parseInt(seconds) - 1;
+    }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      timer.innerHTML = minutes + ":" + seconds;
+      document.title = minutes + ":" + seconds;
   }
 }
